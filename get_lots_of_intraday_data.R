4cbc547818d71803dc1428157d7df9893c03bfd1
abbynoyce <anoyce@Abigails-MacBook-Pro.local>
2015-09-21 14:36:44 -0400

    Improved intraday-data scraper
    Better! Splits hours, minutes, days of week in useful ways.

--- get_lots_of_intraday_data.R
+++ get_lots_of_intraday_data.R
@@ -3,7 +3,7 @@
 # you want to scrape.
 
 # Create cookie w/ your fitbit account login info
-cookie <- login(email="fitbit login email", password = "fitbit password")
+#cookie <- login(email="fitbit login email", password = "fitbit password")
 
 # Specify dates and data
 start_date <- as.Date("2015-01-01")
@@ -13,7 +13,7 @@ data_to_get = "steps" # or "distance", "floors", "active-minutes", "calories-bur
 # ------ Edit above this line, shouldn't need to edit below -------------
 
 # Initialize empty data frame
-all_data = data.frame(day=as.Date(character()), time=character(), steps=integer(), stringsAsFactors=FALSE)
+steps_df = data.frame(day=as.Date(character()), time=character(), steps=integer(), stringsAsFactors=FALSE)
 
 # Loop over days and scrape
 date_seq = seq(start_date, to = end_date, by = "day")
@@ -23,12 +23,13 @@ while (i <= length(date_seq)) {
   # Scraper call
   one_day_data = get_intraday_data(cookie, "steps", date = as.character(date_seq[i])) 
   i = i+1
-  
-  # Pull date and time into separate columns
-  one_day_data$day = format(one_day_data$time, format = "%Y-%m-%d")
-  one_day_data$time = format(one_day_data$time, format = "%H:%M:%S")
-  
   # Combine across days
-  all_data = rbind(all_data, one_day_data)
+  steps_df = rbind(steps_df, one_day_data)
 }
+
+# Pull date and time into separate columns
+steps_df$date = format(steps_df$time, format = "%Y-%m-%d")
+steps_df$wday = wday(steps_df$time, label=TRUE, abbr=TRUE)
+steps_df$hour = hour(steps_df$time)
+steps_df$minute = minute(steps_df$time)
 
