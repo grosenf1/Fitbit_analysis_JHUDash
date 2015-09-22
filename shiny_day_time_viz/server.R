05132b938f6476fd369dc6c4585673b6aae96bce
abbynoyce <anoyce@Abigails-MacBook-Pro.local>
2015-09-22 14:12:46 -0400

    Added Day of Week and Time of Day plots

--- shiny_day_time_viz/server.R
+++ shiny_day_time_viz/server.R
@@ -7,19 +7,49 @@ shinyServer(
   function(input, output) {
     #data <- all_data[all_data$id==input$id,]
     
-    output$idText <- renderText({ 
-      paste("Displaying subject", input$id, ":")
-    })
+#     output$idText <- renderText({ 
+#       paste("Displaying subject", input$id, ":")
+#     })
     
-    output$dateText<- renderText({
-      paste(input$start.end.date[1], "until", input$start.end.date[2])
-    })
+#     output$dateText<- renderText({
+#       paste(input$start.end.date[1], "until", input$start.end.date[2])
+#     })
     
     output$stepsPlot <- renderPlot({
+      # Subset data by subject, dates, day of week
       data <- all_data[all_data$id==input$id,]
       data <- data[as.Date(data$date) >= as.Date(input$start.end.date[1]),]
       data <- data[as.Date(data$date) <= as.Date(input$start.end.date[2]),]
+      data <- data[data$DofW.steps %in% input$DofW,]
+      
+      # Plot! Needs to be prettier
       ggplot(data=data,aes(x=time,y=steps,group=1)) + geom_line()
+    })
+    
+    output$stepsDayPlot <- renderPlot({
+      # subset data by subject, dates, day of week
+      data <- all_data[all_data$id==input$id,]
+      data <- data[as.Date(data$date) >= as.Date(input$start.end.date[1]),]
+      data <- data[as.Date(data$date) <= as.Date(input$start.end.date[2]),]
+      data <- data[data$DofW.steps %in% input$DofW,]
+      
+      # Make plot
+      ggplot(data=data, aes(x=factor(DofW.steps), y=tot_steps, fill=time)) +
+        stat_summary(fun.y="mean", geom="bar")
+      
+    })
+    
+    output$stepsTimePlot <- renderPlot({
+      # subset data by subject, dates, day of week
+      data <- all_data[all_data$id==input$id,]
+      data <- data[as.Date(data$date) >= as.Date(input$start.end.date[1]),]
+      data <- data[as.Date(data$date) <= as.Date(input$start.end.date[2]),]
+      data <- data[data$DofW.steps %in% input$DofW,]
+      
+      # Make plot
+      ggplot(data=data, aes(x=factor(TOD), y=4*steps, fill=time)) +
+        stat_summary(fun.y="mean", geom="bar")
+      
     })
     
     
--- shiny_day_time_viz/ui.R
+++ shiny_day_time_viz/ui.R
@@ -3,14 +3,26 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel( 
-      selectInput("id", label = "Subject", c("1","2","3","4","5")),
+      selectInput("id", 
+                  label = "Subject", 
+                  c("1","2","3","4","5")),
       
-      dateRangeInput("start.end.date", label = "Dates", max=today(), start = today()-30)
+      dateRangeInput("start.end.date", 
+                     label = "Dates", 
+                     max=today(), 
+                     start = today()-30),
+      
+      checkboxGroupInput("DofW", 
+                         label="Days of the Week to include", 
+                         choices = list("Monday" = "Mon", "Tuesday" = "Tues", "Wednesday" = "Wed", "Thursday" = "Thurs", "Friday" = "Fri", "Saturday" = "Sat", "Sunday" = "Sun"), 
+                         selected = c("Mon","Tues","Wed","Thurs","Fri"))
     ),
     mainPanel(
       textOutput("idText"),
       textOutput("dateText"),
-      plotOutput("stepsPlot")
+      plotOutput("stepsPlot"),
+      plotOutput("stepsDayPlot"),
+      plotOutput("stepsTimePlot")
       )
   )
   
