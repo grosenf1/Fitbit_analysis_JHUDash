a0ad7e8c19cfac3b002935e706ee124577cbce12
abbynoyce <anoyce@Abigails-MacBook-Pro.local>
2015-09-22 13:07:36 -0400

    Date range functionality added
    You can now select a date range for the plot; also now includes the
    “good” all_data.Rdata file.

--- shiny_day_time_viz/server.R
+++ shiny_day_time_viz/server.R
@@ -11,8 +11,14 @@ shinyServer(
       paste("Displaying subject", input$id, ":")
     })
     
+    output$dateText<- renderText({
+      paste(input$start.end.date[1], "until", input$start.end.date[2])
+    })
+    
     output$stepsPlot <- renderPlot({
       data <- all_data[all_data$id==input$id,]
+      data <- data[as.Date(data$date) >= as.Date(input$start.end.date[1]),]
+      data <- data[as.Date(data$date) <= as.Date(input$start.end.date[2]),]
       ggplot(data=data,aes(x=time,y=steps,group=1)) + geom_line()
     })
     
--- shiny_day_time_viz/ui.R
+++ shiny_day_time_viz/ui.R
@@ -5,10 +5,11 @@ shinyUI(fluidPage(
     sidebarPanel( 
       selectInput("id", label = "Subject", c("1","2","3","4","5")),
       
-      dateRangeInput("start.end.date", label = "Dates (NOT CURRENTLY FUNCTIONAL")
+      dateRangeInput("start.end.date", label = "Dates", max=today(), start = today()-30)
     ),
     mainPanel(
       textOutput("idText"),
+      textOutput("dateText"),
       plotOutput("stepsPlot")
       )
   )
