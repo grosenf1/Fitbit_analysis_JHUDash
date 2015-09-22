f5f973e97ce25c82849d77ff8135a72fa6056279
Gabriel <gabriel.e.rosenfeld@gmail.com>
2015-09-22 09:29:38 -0400

    new commit

---
+++ get_sleep_data.R
@@ -0,0 +1,25 @@
+# get sleep data script
+# inputs user name
+# outputs sleep data frame to /data directory
+
+library(lubridate)
+# change this setting for git working directory
+# setwd("~/dev/R/Fitbit_analysis_JHUDash/")
+
+# Create cookie w/ your fitbit account login info
+# cookie <- login(email="fitbit login email", password = "fitbit password")
+
+cat(paste0("Current working directory is: ", getwd()))
+# get the user name or id for thier step data
+user_id <- readline("What is your name or id? ")
+
+# write file to /data with user_id appended
+file.name <- paste0("data", "/", user_id, "_", "sleep.csv")
+
+# pull sleep data from fitbit
+sleep <- get_sleep_data(cookie, start_date = "2015-09-01", end_date = "2015-09-20")
+
+# convert sleep list() to a data frame
+sleep_df <- data.frame(lapply(sleep$df, as.character), stringsAsFactors=FALSE)
+
+write.csv(sleep_df, file = file.name, row.names = F)
--- merge_datasets.R
+++ merge_datasets.R
@@ -3,37 +3,93 @@
 # Work in progress: Selects the correct columns from each subject's data file and adds a subject id.
 # Needs: Merge across two frames for each subject. Merge frames across multiple subjects.
 
-
-
-subject_data <- list(list("data/jtfsteps.csv","data/jtfsleep.csv"), list("data/chris_steps.csv","data/chris_sleep.csv"), list("data/stepsDLC.csv","data/sleepDLC.csv"))
+#subject_data <- list(#list("data/jtfsteps.csv","data/jtfsleep.csv"), 
+#                     list("data/chris_steps.csv","data/chris_sleep.csv"), 
+#                     list("data/stepsDLC.csv","data/sleepDLC.csv"))
 #subject_data <- list(list("data/stepsDLC.csv","data/sleepDLC.csv"))
 # list("data/an_steps.csv","") # This subject has no sleep file; doesn't currently handle gracefully
 
-# Set appropriate working directory
-setwd("~/Desktop/Fitbit_analysis_JHUDash/")
-
 library(lubridate)
-library(dplyr, type="source")
+library(dplyr)
 
-subject_ids <- seq(1,length(subject_data))
+# Set appropriate working directory
+#setwd("~/dev/R/Fitbit_analysis_JHUDash/")
+cat(paste0("Current working directory is: ", getwd()))
 
-for (s in subject_ids) {
-  # steps
-  steps <- read.csv(subject_data[[s]][[1]]) # step data
-  steps_final_format <- data.frame(
-    subjID <- rep(s,dim(steps)[1]),
-    date <- steps$date,
-    DofW <- steps$wday,
-    HofD <- steps$hour,
-    MofH <- steps$minute,
-    stepcount <- steps$steps
-  )
-  
-  sleep <- read.csv(subject_data[[s]][[2]]) # sleep data
-  sleep_final_format = data.frame(
-    date <- sleep$date,
-    sleepqualA <- sleep$sleepQualityScoreA,
-    sleepDuration <- sleep$sleepDuration,
-    minAsleep <- sleep$minAsleep
-  )
+steps_format <- function(x, f) {
+  steps <- read.csv(f, row.names = NULL, header = T, stringsAsFactors=F) # step data
+  steps <- cbind(id = x, steps)
+  names(steps) <- c("id", "time", "steps", "date", "DofW", "HofD", "MofH", "month") 
+  steps$time <- ymd_hms(steps$time)
+  steps$date <- ymd(steps$date)
+  steps$DofW <- wday(parse_date_time(steps$time, 
+                                           "Y%m%d %H%M%S"), abbr=T, label=T)
+  # if month label uncomment below
+  # steps$month <- month(steps$month, abbr=T, label=T)
+  steps
 }
+
+sleep_format <- function(x, f) {
+  sleep <- read.csv(f, row.names = NULL, header = T, stringsAsFactors=F) # sleep data
+  sleep <- cbind(id = x, sleep)
+  sleep$date <- ymd(sleep$date)
+  sleep$DofW <- wday(parse_date_time(paste(ymd(sleep$date), 
+                        sleep$startTime),"Y%m%d %H%M"), abbr=T, label=T)
+  sleep$HofD <- hour(parse_date_time(paste(ymd(sleep$date), 
+                                           sleep$startTime),"Y%m%d %H%M"))
+  sleep$MofH <- minute(parse_date_time(paste(ymd(sleep$date), 
+                                           sleep$startTime),"Y%m%d %H%M"))
+  sleep$month <- month(parse_date_time(paste(ymd(sleep$date), 
+                                           sleep$startTime),"Y%m%d %H%M"))
+  sleep
+}
+
+df.steps <- data.frame()
+df.sleep <- data.frame()
+sleep.files <- list.files(path = "data", pattern = "sleep", full.names=T)
+steps.files <- list.files(path = "data", pattern = "step", full.names=T)
+
+sleep.files <- strsplit(sleep.files, split = " ")
+steps.files <- strsplit(steps.files, split = " ")
+
+for (s in 1:length(sleep.files)) {
+  # steps
+  sleep <- sleep_format(s, sleep.files[[s]])
+  df.sleep <- rbind(df.sleep, sleep)
+}
+
+for (s in 1:length(steps.files)) {
+  steps <- steps_format(s, steps.files[[s]])
+  df.steps <- rbind(df.steps, steps)
+}
+
+all_data <- inner_join(df.steps, df.sleep, by="date")
+
+names(all_data) <- c("id", "time", "steps", "date", "DofW.steps",
+                     "HofD.steps", "MofH.steps", "month.steps",
+                     "id.sleep", "sleep.start", "sleep.end", "sleep.duration",
+                     "awoken.count", "restless.count", "awake.time",
+                     "restless.time", "min.asleep", "sleepQuality.B",
+                     "sleepQuality.A", "DofW.sleep", "HofD.sleep", "MofH.sleep",
+                     "month.sleep")
+
+save(all_data, file = "data/all_data.Rdata")
+# Note: I have no ordered the data at this point of checked for sort order
+#
+# usage below
+
+# filter all_data by a single day, hours of sleep, and user id == 1
+filter(all_data, date == ymd("2015-09-07") & HofD.sleep == 2 & id == 1)
+
+# filter by user id and Sept
+# group by date
+# create a total steps column
+# select distinct date, total steps, duration, and quality A
+all_data %>% filter(id == 1 & date > ymd("2015-09-01")) %>%
+  group_by(date) %>%
+  mutate(total_steps = sum(steps)) %>%
+  select(total_steps,
+            sleep.duration,
+            sleepQuality.A) %>%
+  distinct()
+
